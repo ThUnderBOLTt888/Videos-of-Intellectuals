08463ea75c3e4914baae4fd077a8e12641ad2884
ThUnderBOLTt888 <uddhavthakur@bennington.edu>
2026-09-02 07:42:35 -0400

    Add initial hangman game implementation
    Implement basic hangman game structure with stages and word selection.

---
+++ hangman.py
@@ -0,0 +1,67 @@
+import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+display = ""
+
+for letter in chosen_word:
+    display += letter
+print(display)
+
+
+  
